3bb4b210094bcc0c0f4b941d5912a03d2f030473
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-06-19 11:30:41 +0530

    Adding all files

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Products from "./components/Products/Products";
+
+function App() {
+  const [cart, setCart] = useState({});
+
+  function increaseQuantity(product) {
+    let newCart = { ...cart };
+    if (!newCart[product.id]) {
+      newCart[product.id] = { ...product, quantity: 0 };
+    }
+    newCart[product.id].quantity += 1;
+    setCart(newCart);
+  }
+
+  function decreaseQuantity(product) {
+    let newCart = { ...cart };
+    if (!newCart[product.id]) return;
+    newCart[product.id].quantity -= 1;
+    if (newCart[product.id].quantity <= 0) {
+      delete newCart[product.id];
+    }
+    setCart(newCart);
+  }
+
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
