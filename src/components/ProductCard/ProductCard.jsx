71ebe3df48d8a0bf9badf0800814c4791891eecd
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-06-24 12:31:14 +0530

    redux

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,6 @@
 import "./ProductCard.css";
 import AddToCart from "../AddToCart/AddToCart";
+import ReduxAddToCart from '../ReduxAddToCart';
 import { useRef } from "react";
 import PropTypes from 'prop-types';
 
@@ -27,7 +28,13 @@ function ProductCard({ product, cart, increaseQuantity, decreaseQuantity }) {
       <p ref={pRef} style={{display:"none"}}>{product.price.value} {product.price.currency}</p>
       {/* <input type="text" onChange={displayOutput} ref={iRef} value={inputV} /> */}
       {/* <p ref={oRef}>Over here the output would arrive - {inputV}</p> */}
-      <AddToCart
+      {/* <AddToCart
+        product={product}
+        cart={cart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+      /> */}
+      <ReduxAddToCart
         product={product}
         cart={cart}
         increaseQuantity={increaseQuantity}
